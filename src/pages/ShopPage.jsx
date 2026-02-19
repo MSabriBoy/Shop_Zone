41e3ff460136c1aedea890fb37567052a7129ac5
Rihan Sabri <mrihansabri@gmail.com>
2026-02-20 00:27:24 +0530

    shop page file's logic has completed and its is working too

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,5 +1,29 @@
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 export function ShopPage() {
+    const [products, setProducts]= useState([]);
+
+    useEffect(()=>{
+        fetch("https://dummyjson.com/products")
+        .then((response)=>response.json())
+        .then((data)=>{
+            setProducts(data.products);
+        })
+        .catch((error)=>{
+            console.log("Error fetching Product:", error);
+        });
+    }, []);
     return (
-        <h1>Shop Page</h1>
+    <div className="product-grid">
+
+        {products.map((product)=>(
+            <div className="product-card" key={product.id}>
+                <img src={product.thumbnail} alt={product.title} />
+                <h3>{product.title}</h3>
+                <p className="price">${product.price}</p>
+                <Link to={`/product/${product.id}`}>view Details</Link>
+            </div>
+        ))}
+    </div>
     )
 }
