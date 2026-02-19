ac032a5f562cdbcfc2fb7341a335ad4d883c7f64
Rihan Sabri <mrihansabri@gmail.com>
2026-02-20 00:26:18 +0530

    HomePages.jsx has created and complete its ui and linked too

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom"
+
 function HomePage() {
     return (
-        <div className="home-page-container" >
-            <h1> hom</h1>
+        <div className="home-container" >
+            <h1>
+                Welcome to our ShopZone
+            </h1>
+            <p className="description">
+                Discover high-quality products at unbeatable prices.
+                Browse our collection, explore detailed product information,
+                and enjoy a smooth shopping experience — all without page reloads.
+            </p>
+
+            <Link to="/shop">
+                <button className="home-btn">Start Shopping</button>
+            </Link>
 
         </div>
     )
